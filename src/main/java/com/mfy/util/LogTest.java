11b1a999e7fa3b2a7257da1dd922ec6392724d50
huangcangbai <huangcangbai@tuniu.com>
2014-04-04 16:32:52 +0800

    dump test

--- src/main/java/com/mfy/util/LogTest.java
+++ src/main/java/com/mfy/util/LogTest.java
@@ -1,14 +1,17 @@
 package com.mfy.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 public class LogTest {
 
-	static Logger LOG = LoggerFactory.getLogger(LogTest.class);
+	///static Logger LOG = LoggerFactory.getLogger(LogTest.class);
 	
 	public static void main(String[] args) {
-		LOG.info("log back test.");
+		
+		int[] b=new int[200*1000000];
+		//LOG.info("log back test." + b);
+		System.out.println(b);
 	}
 
 }
