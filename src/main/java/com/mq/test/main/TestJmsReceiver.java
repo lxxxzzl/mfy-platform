b23330ca6533680a48a2b486c43575ecfd0f9520
huangcangbai <huangcangbai@tuniu.com>
2014-04-14 18:03:58 +0800

    解决logback无法使用问题

--- src/main/java/com/mq/test/main/TestJmsReceiver.java
+++ src/main/java/com/mq/test/main/TestJmsReceiver.java
@@ -2,50 +2,50 @@ package com.mq.test.main;
 
 import java.util.Date;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
-import javax.jms.MapMessage;
-import javax.jms.MessageConsumer;
-import javax.jms.Session;
-
-import org.apache.activemq.ActiveMQConnectionFactory;
+//import javax.jms.Connection;
+//import javax.jms.ConnectionFactory;
+//import javax.jms.Destination;
+//import javax.jms.MapMessage;
+//import javax.jms.MessageConsumer;
+//import javax.jms.Session;
+//
+//import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class TestJmsReceiver {
-    public static void main(String[] args) throws Exception {  
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory();  
-      
-        Connection connection = connectionFactory.createConnection();  
-        connection.start();  
-      
-        final Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);  
-        Destination destination = session.createQueue("my-queue");  
-      
-        MessageConsumer consumer = session.createConsumer(destination);  
-        /*//listener 方式 
-        consumer.setMessageListener(new MessageListener() { 
-     
-            public void onMessage(Message msg) { 
-                MapMessage message = (MapMessage) msg; 
-                //TODO something.... 
-                System.out.println("收到消息：" + new Date(message.getLong("count"))); 
-                session.commit(); 
-            } 
-     
-        }); 
-        Thread.sleep(30000); 
-        */  
-        int i=0;  
-        while(i<3) {  
-            i++;  
-            MapMessage message = (MapMessage) consumer.receive();  
-            session.commit();  
-      
-            //TODO something....  
-            System.out.println("收到消息：" + new Date(message.getLong("count")));  
-        }  
-      
-        session.close();  
-        connection.close();  
-    }  
+//    public static void main(String[] args) throws Exception {  
+//        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory();  
+//      
+//        Connection connection = connectionFactory.createConnection();  
+//        connection.start();  
+//      
+//        final Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);  
+//        Destination destination = session.createQueue("my-queue");  
+//      
+//        MessageConsumer consumer = session.createConsumer(destination);  
+//        /*//listener 方式 
+//        consumer.setMessageListener(new MessageListener() { 
+//     
+//            public void onMessage(Message msg) { 
+//                MapMessage message = (MapMessage) msg; 
+//                //TODO something.... 
+//                System.out.println("收到消息：" + new Date(message.getLong("count"))); 
+//                session.commit(); 
+//            } 
+//     
+//        }); 
+//        Thread.sleep(30000); 
+//        */  
+//        int i=0;  
+//        while(i<3) {  
+//            i++;  
+//            MapMessage message = (MapMessage) consumer.receive();  
+//            session.commit();  
+//      
+//            //TODO something....  
+//            System.out.println("收到消息：" + new Date(message.getLong("count")));  
+//        }  
+//      
+//        session.close();  
+//        connection.close();  
+//    }  
 }
--- src/main/java/com/mq/test/spring/TestSpringJmsSender.java
+++ src/main/java/com/mq/test/spring/TestSpringJmsSender.java
@@ -2,10 +2,10 @@ package com.mq.test.spring;
 
 import java.util.Date;
 
-import javax.jms.JMSException;
-import javax.jms.MapMessage;
-import javax.jms.Message;
-import javax.jms.Session;
+//import javax.jms.JMSException;
+//import javax.jms.MapMessage;
+//import javax.jms.Message;
+//import javax.jms.Session;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
@@ -15,15 +15,15 @@ import org.springframework.jms.core.MessageCreator;
 public class TestSpringJmsSender {
 	
     public static void main(String[] args) throws Exception {
-        ApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:beanDefines*.xml");  
-        JmsTemplate jmsTemplate = (JmsTemplate) ctx.getBean("jmsTemplate");  
-        jmsTemplate.send(new MessageCreator() {  
-            public Message createMessage(Session session) throws JMSException {  
-                MapMessage mm = session.createMapMessage();  
-                mm.setLong("count", new Date().getTime());  
-                return mm;  
-            }  
-        });  
+//        ApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:beanDefines*.xml");  
+//        JmsTemplate jmsTemplate = (JmsTemplate) ctx.getBean("jmsTemplate");  
+//        jmsTemplate.send(new MessageCreator() {  
+//            public Message createMessage(Session session) throws JMSException {  
+//                MapMessage mm = session.createMapMessage();  
+//                mm.setLong("count", new Date().getTime());  
+//                return mm;  
+//            }  
+//        });  
     }  
     
 }
