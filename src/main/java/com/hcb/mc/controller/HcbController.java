9a5a4e58625e7788456ece85f764c016f48fa67b
huangcangbai <huangcangbai@tuniu.com>
2014-05-17 17:13:01 +0800

    添加 spring mvc的标签 ResponseStatus(HttpStatus.NO_CONTENT)--void

--- src/main/java/com/hcb/mc/controller/HcbController.java
+++ src/main/java/com/hcb/mc/controller/HcbController.java
@@ -3,9 +3,11 @@ package com.hcb.mc.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.hcb.mc.bean.MutliBean;
 import com.hcb.mc.util.BeanUtil;
@@ -15,7 +17,8 @@ import com.hcb.mc.util.BeanUtil;
 public class HcbController {
 
     @RequestMapping( value="/test/{id}" )
-    public String test(HttpServletRequest request,HttpServletResponse response,@PathVariable String id){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void test(HttpServletRequest request,HttpServletResponse response,@PathVariable String id){
     	BeanUtil beanUtil = (BeanUtil) BeanUtil.applicationContext.getBean("beanUtil");
 		
 		System.out.println("beanUtil.getPlayer(): " + beanUtil.getPlayer());
@@ -25,7 +28,6 @@ public class HcbController {
 		
 		MutliBean mutliBean = beanUtil.getBeanInterfaceFactory().getBean("a");
 		mutliBean.methodA();
-        return "ok";
     }
 	
 }
