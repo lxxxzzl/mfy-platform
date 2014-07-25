f046d7cff3b64fb7bef422f10dec57586da54623
huangcangbai <huangcangbai1@163.com>
2014-07-25 10:55:54 +0800

    添加spring线程池测试代码

---
+++ src/main/java/com/thread/TaskExecutorExample.java
@@ -0,0 +1,35 @@
+package com.thread;
+
+import org.springframework.core.task.TaskExecutor;
+
+public class TaskExecutorExample {
+	private TaskExecutor taskExecutor;
+
+	public TaskExecutorExample(TaskExecutor taskExecutor) {
+		this.taskExecutor = taskExecutor;
+	}
+
+	public void printMessages() {
+		for (int i = 0; i < 600; i++) {
+			taskExecutor.execute(new MessagePrinterTask("Message" + i));
+		}
+	}
+
+	private class MessagePrinterTask implements Runnable {
+		private String message;
+
+		public MessagePrinterTask(String message) {
+			this.message = message;
+		}
+
+		public void run() {
+			System.out.println(message + " start");
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(message + " end");
+		}
+	}
+}
---
+++ src/main/java/com/thread/ThreadController.java
@@ -0,0 +1,42 @@
+package com.thread;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@RequestMapping(value = "/thread")
+public class ThreadController implements ApplicationContextAware{
+
+	static Logger LOG = LoggerFactory.getLogger(ThreadController.class);
+	
+	private ApplicationContext applicationContext;
+	
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException { 
+		this.applicationContext = applicationContext;
+	}
+    
+    /**
+     * localhost:8080/mfy-platform/thread/test/
+     */
+    @RequestMapping(value="/test") 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void test(){
+    	TaskExecutorExample te = (TaskExecutorExample)applicationContext.getBean("taskExecutorExample");   
+    	te.printMessages();   
+    	System.out.println("新请求进入");  
+    }
+    
+}
+
+
