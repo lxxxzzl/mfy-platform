82584f2058de61ca4a8ed6f719b20a2b9232e6f5
huangcangbai <huangcangbai1@163.com>
2014-12-30 13:45:56 +0800

    声明Spring容器关闭时，关闭数据源的方法

---
+++ bin/sql/test.sql
@@ -0,0 +1,34 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost2
+Source Server Version : 50614
+Source Host           : 127.0.0.1:3310
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50614
+File Encoding         : 65001
+
+Date: 2014-03-18 18:10:55
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `op_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', '张三');
+INSERT INTO `user` VALUES ('2', '李四');
+INSERT INTO `user` VALUES ('3', '王五');
